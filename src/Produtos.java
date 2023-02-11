4362fabfc548c57c712389f507a70263f92cc06d
mateuslph <mlptche@gmail.com>
2023-02-10 21:38:28 -0300

    inicio da atividade 2

---
+++ src/Produtos.java
@@ -0,0 +1,34 @@
+public class Produtos {
+    private int idProduto;
+    private String descricao;
+    private float preco;
+    private String unidade;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+}
