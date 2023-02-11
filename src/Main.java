851193b1b98c5f0a7f3b3374deee89121f01b532
mateuslph <mlptche@gmail.com>
2023-02-11 13:47:49 -0300

    Adicionado connector jdbc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.*;
+
 public class Main {
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/senai_uc10_sa2";
     public static void main(String[] args) {
-        
+        Connection connection = null;
+        Statement stm = null;
+        ResultSet rs = null;
+
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, "root", "10.20-Ma.login");
+            stm = connection.createStatement();
+            rs = stm.executeQuery("select * from produtos");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + " " +
+                        rs.getString(2) + " " +
+                        rs.getFloat(3) + " " +
+                        rs.getString(4));
+            connection.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
     }
 }
